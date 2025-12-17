980604f1a554107aa7d885067c6d6017adbc8794
gideondakore <dakoregideon72@gmail.com>
2025-12-17 16:52:46 +0000

    Working with network

---
+++ main/java/com/amalitech/testing/networks/FirstNetWork.java
@@ -0,0 +1,10 @@
+package com.amalitech.testing.networks;
+
+import java.net.InetSocketAddress;
+
+public class FirstNetWork {
+
+    public static void main(String[] args){
+//        InetSocketAddress
+    }
+}
