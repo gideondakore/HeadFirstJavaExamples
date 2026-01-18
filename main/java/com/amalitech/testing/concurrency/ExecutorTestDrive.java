3208bfe03c12eec938da1ae124e24e8778327084
gideondakore <dakoregideon72@gmail.com>
2026-01-18 10:39:51 -0500

    chore: implemted latch count down

--- main/java/com/amalitech/testing/concurrency/ExecutorTestDrive.java
+++ main/java/com/amalitech/testing/concurrency/ExecutorTestDrive.java
@@ -11,7 +11,8 @@ public class ExecutorTestDrive {
 
         try(ExecutorService executor = Executors.newSingleThreadExecutor();){
 
-            executor.submit(() -> IO.println("Top  o' the stack"));
+            executor.execute(() -> IO.println("Top  o' the stack")); // Use execute() when you just want to run a task and don't care about the result or completion status: UncaughtExceptionHandler
+//            executor.submit(() -> IO.println("Top  o' the stack")); // Use submit() when you need the result, want to handle exceptions, or need to check/control task execution : Can catch via Future.get()
             IO.println("back in main");
 
             executor.shutdown();
