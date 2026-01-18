00b4272855b11e5589e52696f13c6980bda18ebe
gideondakore <dakoregideon72@gmail.com>
2026-01-18 13:44:35 -0500

    feature: simulate lost update in concurrency

---
+++ main/java/com/amalitech/testing/concurrency/LostUpdate.java
@@ -0,0 +1,30 @@
+package com.amalitech.testing.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class LostUpdate {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(6);
+        Balance balance = new Balance();
+        for (int i = 0; i < 1000; i++) {
+            pool.execute(balance::increment);
+        }
+        pool.shutdown();
+        if (pool.awaitTermination(1, TimeUnit.MINUTES)) {
+            System.out.println("balance = " + balance.bal);
+
+        }
+
+    }
+}
+class Balance {
+    int bal = 0;
+
+    public void increment() {
+        bal++;
+    }
+
+}
+
