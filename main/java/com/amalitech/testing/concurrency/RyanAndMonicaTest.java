d745c3214580383a06c23233ef88cc3a58b4e932
gideondakore <dakoregideon72@gmail.com>
2026-01-23 15:15:03 +0000

    chore: add the implementation in the book

--- main/java/com/amalitech/testing/concurrency/RyanAndMonicaTest.java
+++ main/java/com/amalitech/testing/concurrency/RyanAndMonicaTest.java
@@ -7,8 +7,11 @@ public class RyanAndMonicaTest {
 
     public static void main(String[] args) {
 
-        BankAccount account = new BankAccount();
+//        BankAccount account = new BankAccount();
 //        BankAccountAtomicOperation account = new BankAccountAtomicOperation();
+        BankAccountAtomicOperation2 account = new BankAccountAtomicOperation2();
+
+
 
 
         RyanAndMonicaJob ryan = new RyanAndMonicaJob("Ryan", account, 50);
