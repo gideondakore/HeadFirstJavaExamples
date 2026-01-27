80f3eb648f4a834870ad44f59c97a2d926f9f809
gideondakore <dakoregideon72@gmail.com>
2026-01-27 09:49:42 +0000

    feature: add alternative args

--- main/java/com/amalitech/headfirstjava/varags/VarAgs.java
+++ main/java/com/amalitech/headfirstjava/varags/VarAgs.java
@@ -3,10 +3,18 @@ package com.amalitech.headfirstjava.varags;
 public class VarAgs {
 
     public static void main(String[] args){
-        new VarAgs().printAllObjects((Object[])args);
+//        new VarAgs().printAllObjects((Object[])args);
+        new VarAgs().printAllObjects2((Object[])args);
+
     }
 
     public void printAllObjects(Object... elements){
+        for(Object element: elements){
+            IO.println(element);
+        }
+    }
+
+    public void printAllObjects2(Object[] elements){
         for(Object element: elements){
             IO.println(element);
         }
