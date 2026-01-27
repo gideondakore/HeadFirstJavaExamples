9004215b4b76e1dfe463fc456d17d6f863802f17
gideondakore <dakoregideon72@gmail.com>
2026-01-27 09:55:15 +0000

    fix: now using both alternatives

--- main/java/com/amalitech/headfirstjava/varags/VarAgs.java
+++ main/java/com/amalitech/headfirstjava/varags/VarAgs.java
@@ -2,11 +2,17 @@ package com.amalitech.headfirstjava.varags;
 
 public class VarAgs {
 
-    public static void main(String[] args){
-//        new VarAgs().printAllObjects((Object[])args);
-        new VarAgs().printAllObjects2((Object[])args);
+    public static void main(String... args){
+        new VarAgs().printAllObjects((Object[])args);
+//        new VarAgs().printAllObjects2(args);
 
     }
+
+//    public static void main(String[] args){
+//        new VarAgs().printAllObjects((Object[])args);
+//        new VarAgs().printAllObjects2((Object[])args);
+//
+//    }
 
     public void printAllObjects(Object... elements){
         for(Object element: elements){
