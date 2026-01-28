e9f921857f6a623a7a9c9cca8b7fa0802b1c9b9c
gideondakore <dakoregideon72@gmail.com>
2026-01-28 09:38:51 +0000

    chore: working with enums

--- main/java/com/amalitech/headfirstjava/appendix/EnumClass.java
+++ main/java/com/amalitech/headfirstjava/appendix/EnumClass.java
@@ -1,6 +1,11 @@
 package com.amalitech.headfirstjava.appendix;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public enum EnumClass{
     JOHN("John Lennon", "Rhythm Guitar"),
     PAUL("Paul McCartney", "Bass"),
@@ -9,6 +14,8 @@ public enum EnumClass{
 
     private final String fullName;
     private final String instrument;
+
+    private static final EnumClass[] $VALUES = {JOHN, PAUL, GEORGE, RINGO};
 
     EnumClass(String fullName, String instrument) {
         this.fullName = fullName;
@@ -19,7 +26,26 @@ public enum EnumClass{
         return fullName;
     }
 
+
     public String getInstrument() {
         return instrument;
+    }
+
+    public static EnumClass[] enumValues(){
+        return $VALUES.clone();
+    }
+
+    //Outside code can modify the original enum
+    public static EnumClass[] enumValuesNotSafe(){
+        return $VALUES;
+    }
+
+
+    public static EnumClass getJohn(){
+        return JOHN;
+    }
+
+    public static List<EnumClass> listValues(){
+        return Collections.unmodifiableList(Arrays.asList($VALUES));
     }
 }
--- main/java/com/amalitech/headfirstjava/appendix/Main.java
+++ main/java/com/amalitech/headfirstjava/appendix/Main.java
@@ -1,10 +1,13 @@
 package com.amalitech.headfirstjava.appendix;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String... args){
         EnumClass member = EnumClass.valueOf("JOHN");
+
         IO.println("MEMBER FULL NAME: " + member.getFullName());
         IO.println("MEMBER FULL INSTRUMENT NAME: " + member.getInstrument());
         IO.println("MEMBERS: " + Arrays.toString(EnumClass.values()));
@@ -13,5 +16,32 @@ public class Main {
         for(EnumClass mem: EnumClass.values()){
             IO.println("member: " + mem.getFullName() + ", plays " + mem.getInstrument() + " instrument");
         }
+
+        IO.println("BEFORE: " + Arrays.toString(EnumClass.values()));
+        IO.println("BEFORE: " + Arrays.toString(EnumClass.enumValues()));
+
+
+
+        EnumClass john = EnumClass.getJohn();
+        john = EnumClass.PAUL;
+
+        IO.println("EXPECTING PAUL: " + john);
+        IO.println("AFTER" + Arrays.toString(EnumClass.enumValues()));
+
+
+        EnumClass[] members = EnumClass.enumValues();
+        members[0] = null;
+        IO.println(Arrays.toString(EnumClass.enumValues()));
+
+        EnumClass[] members2 = EnumClass.enumValues();
+        IO.println(members2[0]);
+
+        List<EnumClass> membersList = new ArrayList<>(EnumClass.listValues());
+        membersList.set(0, null);
+        IO.println(Arrays.toString(EnumClass.enumValues()));
+
+        List<EnumClass> members2List =new ArrayList<>(EnumClass.listValues());
+        IO.println(members2List.getFirst());
+
     }
 }
