af96f07d9f9b88e02a2489c617320e103a1148a9
gideondakore <dakoregideon72@gmail.com>
2026-01-28 08:08:44 +0000

    chore: try getting the String from the Enum

--- main/java/com/amalitech/headfirstjava/appendix/SomeClass.java
+++ main/java/com/amalitech/headfirstjava/appendix/SomeClass.java
@@ -14,6 +14,15 @@ public class SomeClass {
         if(selectedBandMember == Member.KEVIN){
             IO.println("MEMBER: " + Arrays.toString(Member.values()));
         }
+        // instanceof checks inheritance; class comparison does not
+        Object obj = selectedBandMember;
+        String member = selectedBandMember.name();
+        IO.println("GET STRING VALUE OF MEMBER ENUM: " + (member instanceof String));
+        boolean isString = obj instanceof String;
         IO.println("SELECTED MEMBER: " + selectedBandMember);
+        IO.println("INSTANCE OF STRING: " + isString);
+        IO.println("INSTANCE OF ENUM MEMBER TYPE: " + selectedBandMember.getClass().isEnum());
+        IO.println("INSTANCE OF TYPE: " + selectedBandMember.getClass().getName());
+        IO.println("INSTANCE OF SIMPLIFY TYPE: " + selectedBandMember.getClass().getSimpleName());
     }
 }
