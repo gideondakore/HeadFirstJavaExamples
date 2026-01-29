a98fe523407d060c839eed8548f7f79373bce0eb
gideondakore <dakoregideon72@gmail.com>
2026-01-29 08:08:30 +0000

    feature: start with Record

--- main/java/com/amalitech/headfirstjava/appendix/EnumMain.java
+++ main/java/com/amalitech/headfirstjava/appendix/EnumMain.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Main {
+public class EnumMain {
     public static void main(String... args){
         EnumClass member = EnumClass.valueOf("JOHN");
 
